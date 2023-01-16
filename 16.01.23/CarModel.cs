903d6bbafb3654bafbe09192cb2b5d7fd2e656ba
Irfan Yanbukhtin <woolfyko@gmail.com>
2023-01-16 21:45:20 +0400

    Prop.Const

---
+++ 16.01.23/CarModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _16._01._23
+{
+    internal class Car
+    {
+       
+        public string Name { get; set; }
+        
+        public int MaxSpeed { get; set; }
+
+        public int Year { get; set; }
+
+
+        /////// <summary>
+       public Car(string name, string model, int maxSpeed, int year)
+        {
+            Name = name;
+            _model = model;
+            MaxSpeed = maxSpeed;
+            Year = year;
+        }
+
+        //////
+        private string _model;
+
+        public string Model
+        {
+            get => _model;
+            set
+            {
+                Console.WriteLine("You assigned this model :" + value);
+                _model = value;
+            }
+        }
+
+        internal void Print()
+        {
+            Console.WriteLine($"{Name} {Year} {Model} {MaxSpeed}");
+        }
+    } 
+       
+}
---
+++ 16.01.23/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _16._01._23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var stinger = new Car()
+
+                stinger.Name = "Stinger";
+                stinger.MaxSpeed = 260;
+                stinger.Year = 2016;
+                stinger.Model = "KIA";
+
+
+
+
+
+
+
+
+
+
+            var stinger = new Car 
+
+            {
+                Name = "Stinger",
+                Max.Speed = 260,
+                Year = 2016,
+                Model = "KIA"
+            };
+                 stinger.Print;
+        }
+
+    }
+}
